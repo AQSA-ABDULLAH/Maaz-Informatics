6a5bb4b760ef622464b305641a24a546be4b06a2
AQSA-ABDULLAH <aqsaabdullah38403gmail.com>
2024-08-28 16:55:58 +0500

    complete section 4

--- Client/user/src/pages/chronic-care/ChronicCare.js
+++ Client/user/src/pages/chronic-care/ChronicCare.js
@@ -3,6 +3,7 @@ import styles from './chroniccare.module.css';
 import CareSection1 from '../../component/sections/chronic-care/section1/CareSection1';
 import CareSection2 from '../../component/sections/chronic-care/section2/CareSection2';
 import CareSection3 from '../../component/sections/chronic-care/section3/CareSection3';
+import CareSection4 from '../../component/sections/chronic-care/section4/CareSection4';
 
 function ChronicCare() {
   return (
@@ -11,6 +12,7 @@ function ChronicCare() {
                 <CareSection1 />
                 <CareSection2 />
                 <CareSection3 />
+                <CareSection4 />
             </div>
         </>
   )
