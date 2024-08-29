188f435fe8d9a7fc041e81d3594ead5f38a5379a
AQSA-ABDULLAH <aqsaabdullah38403gmail.com>
2024-08-29 13:34:17 +0500

    create section 3

--- maazweb/Client/user/src/pages/clinical-evidence/ClinicalEvidence.js
+++ maazweb/Client/user/src/pages/clinical-evidence/ClinicalEvidence.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './clinicalevidence.module.css';
 import ClinicalSection1 from '../../component/sections/clinical-evidence/section1/ClinicalSection1';
 import ClinicalSection2 from '../../component/sections/clinical-evidence/section2/ClinicalSection2';
+import ClinicalSection3 from '../../component/sections/clinical-evidence/section3/ClinicalSection3';
 
 export default function ClinicalEvidence() {
     return (
@@ -9,6 +10,7 @@ export default function ClinicalEvidence() {
             <div className={styles.homeContainer}>
                 <ClinicalSection1 />
                 <ClinicalSection2 />
+                <ClinicalSection3 />
             </div>
         </>
     )
