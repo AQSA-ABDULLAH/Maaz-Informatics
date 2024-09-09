f018ddda10ddbc3a4216039db028b09a59bc618f
AQSA-ABDULLAH <aqsaabdullah38403gmail.com>
2024-09-09 16:53:21 +0500

    get blogs post

--- maazweb/Client/user/src/pages/blogs&News/BlogsNews.js
+++ maazweb/Client/user/src/pages/blogs&News/BlogsNews.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import style from './blogsNews.module.css'
-import Blogsection1 from '../../components/sections/blog&new/blog&new1section/Blogs'
-import Blogsection2 from '../../components/sections/blog&new/blog&new2section/Blogfilter'
+import Blogsection1 from '../../component/sections/blog&new/blog&new1section/Blogs'
+import Blogsection2 from '../../component/sections/blog&new/blog&new2section/Blogfilter'
 
 export default function BlogsNews() {
   return (
     <>
-      <div>
-        <img className={style.image} src="/assets/images/blogs&News/blogImage2.png" />
+      <div className={style.homeContainer}>
+        <img className={style.image} src="/assest/images/blogs&News/blogImage2.png"/>
       </div>
       <div className={style.blogSection}>
         <div className={style.blogContainer}>
