c7a4e8900dd331522ec6b2311ef93b980aec3615
AQSA-ABDULLAH <aqsaabdullah38403gmail.com>
2024-09-18 12:33:33 +0500

    create login page

--- maazweb/Client/user/src/component/molecules/login-model/login/Login.js
+++ maazweb/Client/user/src/component/molecules/login-model/login/Login.js
@@ -3,7 +3,7 @@ import Button from "../../../atoms/button/Button";
 import { useDispatch, useSelector } from "react-redux";
 import { signUpWithEmail } from "../../../../redux/containers/auth/actions";
 import { Link, useNavigate } from "react-router-dom";
-import Swal from 'sweetalert2';  // Import SweetAlert2
+import Swal from 'sweetalert2';
 import styles from "./login.module.css";
 
 function Login({ handleSignupClick, onClose }) {  // Add onClose here
@@ -27,14 +27,12 @@ function Login({ handleSignupClick, onClose }) {  // Add onClose here
 
         try {
             await dispatch(signUpWithEmail(loginData));
-            if (reduxState.isSignedIn) {
-                Swal.fire(
-                    'Login Successful!',
-                    'You have successfully logged in.',
-                    'success'
-                );
-                onClose();
-            }
+            Swal.fire(
+                'Login Successful!',
+                'You have successfully logged in.',
+                'success'
+            );
+            onClose();
         } catch (error) {
             Swal.fire(
                 'Login Failed!',
@@ -44,60 +42,60 @@ function Login({ handleSignupClick, onClose }) {  // Add onClose here
         }
     }
 
-        const togglePasswordVisibility = () => {
-            setShowPassword((prevShowPassword) => !prevShowPassword);
-        };
-        
+    const togglePasswordVisibility = () => {
+        setShowPassword((prevShowPassword) => !prevShowPassword);
+    };
 
-        return (
-            <form className="login-form" onSubmit={handleSubmit}>
-                <div>Welcome Back!</div>
-                <p className="subtitle">Stay up-to-date with the latest news by signing in to our newsletter.</p>
+
+    return (
+        <form className="login-form" onSubmit={handleSubmit}>
+            <div>Welcome Back!</div>
+            <p className="subtitle">Stay up-to-date with the latest news by signing in to our newsletter.</p>
+            <input
+                name="email"
+                type="text"
+                placeholder="Email"
+                required
+                onChange={handleChange}
+                value={loginData.email}
+            />
+            <div id="passwordInput">
                 <input
-                    name="email"
-                    type="text"
-                    placeholder="Email"
+                    name="password"
+                    type={showPassword ? "text" : "password"}
+                    placeholder="Password"
                     required
                     onChange={handleChange}
-                    value={loginData.email}
+                    value={loginData.password}
                 />
-                <div id="passwordInput">
-                    <input
-                        name="password"
-                        type={showPassword ? "text" : "password"}
-                        placeholder="Password"
-                        required
-                        onChange={handleChange}
-                        value={loginData.password}
-                    />
-                    <img
-                        className={styles.password_toggle_icon}
-                        alt="Toggle visibility"
-                        onClick={togglePasswordVisibility}
-                        src={process.env.PUBLIC_URL + "/assest/images/login-model/View_icon.png"}
-                    />
-                </div>
-                <label>
-                    <input type="checkbox" name="keepLoggedIn" />
-                    Keep me logged in
-                    <Link to="/forgot-password">Forgot Password?</Link>
-                </label>
+                <img
+                    className={styles.password_toggle_icon}
+                    alt="Toggle visibility"
+                    onClick={togglePasswordVisibility}
+                    src={process.env.PUBLIC_URL + "/assest/images/login-model/View_icon.png"}
+                />
+            </div>
+            <label>
+                <input type="checkbox" name="keepLoggedIn" />
+                Keep me logged in
+                <Link to="/forgot-password">Forgot Password?</Link>
+            </label>
 
-                {/* Update the button to trigger handleSubmit */}
-                <Button btnText="LOGIN" primary radius="0px" type="submit" />
+            {/* Update the button to trigger handleSubmit */}
+            <Button btnText="LOGIN" primary radius="0px" type="submit" />
 
-                <small>
-                    <br />
-                    Don't have an account? <Button
-                        btnText={"Signup"}
-                        textColor={"#9f29bd"}
-                        size={"14px"}
-                        btnClick={handleSignupClick}
-                    />
-                </small>
-            </form>
-        );
-    }
+            <small>
+                <br />
+                Don't have an account? <Button
+                    btnText={"Signup"}
+                    textColor={"#9f29bd"}
+                    size={"14px"}
+                    btnClick={handleSignupClick}
+                />
+            </small>
+        </form>
+    );
+}
 
-    export default Login;
+export default Login;
 
